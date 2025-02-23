0ea4081b598491d8aa908d32ca937d8562199534
spr12ian <173560+spr12ian@users.noreply.github.com>
2025-02-23 10:14:18 +0000

    Minor improvements

--- execute_queries.py
+++ execute_queries.py
@@ -5,7 +5,9 @@ from queries import queries
 if __name__ == "__main__":
     deepseek_nvidia = DeepseekNvidia()
     for query in queries:
-        print(f"Query: {query}")
+        q = query.strip()
+        print(f"Query: {q}")
         print("Response:")
-        response = deepseek_nvidia.get_response(query)
+        response = deepseek_nvidia.get_response(q)
         deepseek_nvidia.print_response(response)
+        print("\n")
